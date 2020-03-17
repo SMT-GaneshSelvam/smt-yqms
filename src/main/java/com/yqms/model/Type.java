45333561463a9b0a9522d02cbc3af24a17113349
DILLI GANESHP <DILLI.GANESHP@lntinfotech.com>
2020-03-17 19:37:15 +0530

    created model classes

---
+++ src/main/java/com/yqms/model/Type.java
@@ -0,0 +1,31 @@
+package com.yqms.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "type")
+public class Type {
+
+	@Id
+	private String id;
+	private String description;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
