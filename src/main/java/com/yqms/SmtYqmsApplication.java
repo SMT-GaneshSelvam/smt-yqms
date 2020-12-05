95cfa1973633f30afe3a850e2f9a55241b5c2777
SMT Ganesh Raghavendra <ganesh_selvam@yahoo.co.in>
2020-12-06 02:35:50 +0530

    Dec 6 Changes

--- src/main/java/com/yqms/SmtYqmsApplication.java
+++ src/main/java/com/yqms/SmtYqmsApplication.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class SmtYqmsApplication {
 
 	public static void main(String[] args) {
-		System.out.println("SMT 786+~ SAT SLT SPT ");
+		System.out.println("SMT 786+~ SAT SLT SPT SST");
 		SpringApplication.run(SmtYqmsApplication.class, args);
 		
 	}
